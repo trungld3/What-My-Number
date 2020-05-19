1e8db58477d3fa5d5ebd72b9ba37de7db093399b
trungld3 <k21tpm4@gmail.com>
2020-05-19 10:42:12 +0700

    UserDefaults.standard

--- What My Number/ViewController.swift
+++ What My Number/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let numberObj = UserDefaults.standard.object(forKey: "number")
+        if let number = numberObj as? String {
+            textField.text = number
+        }
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSave(_ sender: UIButton) {
+        UserDefaults.standard.set(textField.text, forKey: "number")
+    }
+    
 }
 
