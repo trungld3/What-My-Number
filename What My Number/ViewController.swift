41a0ee6bd961e3de3703058f9c60b7e52fa4a7a2
trungld3 <k21tpm4@gmail.com>
2020-05-19 10:00:16 +0700

    Initial Commit

---
+++ What My Number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What My Number
+//
+//  Created by TrungLD on 5/19/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
